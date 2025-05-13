1ad51168eef6c157789e26739ec5f77697d8bcb1
Mathieu Joly <superpose@minimac.local>
2025-05-13 17:14:54 +0200

    add skip animation frames

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,12 +1,10 @@
 import { useEffect, useMemo, useRef, useState } from "react"
+import * as THREE from "three"
+import { useFrame } from "@react-three/fiber"
 import { useAnimations, useGLTF } from "@react-three/drei"
 import { SkeletonUtils } from "three/examples/jsm/Addons.js"
-import { useFrame } from "@react-three/fiber"
-
-import * as THREE from "three"
 
 import { TIME_SCALE, PLATFORM_SPEED } from "../stores/scene"
-
 
 function Model({
   index,
@@ -21,16 +19,19 @@ function Model({
   url: string
   setDuration: (duration: number) => void
 }) {
-  const group = useRef(null)
+  const meshRef = useRef(null)
   const { scene, animations } = useGLTF(url)
   const clone = useMemo(() => SkeletonUtils.clone(scene), [scene])
-  const { actions } = useAnimations(animations, group)
+  const { actions } = useAnimations(animations, clone)
 
   const rotationAngle = useMemo(() => {
     return Math.atan2(Math.cos(angle), Math.sin(angle)) + Math.PI / 2
   }, [angle])
 
+  const frameSkip = useRef(0) // Counter to skip frames
+
   useEffect(() => {
+    // ACTION
     const firstAction = Object.values(actions)[0]
 
     if (!firstAction) {
@@ -45,25 +46,43 @@ function Model({
 
     const firstActionStartTime = (duration * index) / count
     firstAction.time = firstActionStartTime
-    firstAction.timeScale = TIME_SCALE
+    firstAction.timeScale = 0 // Disable automatic time progression
+
+    // skip every 2nd frame
 
     firstAction.play()
   }, [])
 
+  useFrame((state) => {
+    const clock = state.clock.getElapsedTime()
+    if (frameSkip.current % 7 === 0) {
+      // Update animation only every 2nd frame
+      const firstAction = Object.values(actions)[0]
+
+      if (firstAction) {
+        const clip = firstAction.getClip()
+        const duration = clip.duration
+        firstAction.time = clock * TIME_SCALE
+        firstAction.time %= duration // Increment time manually
+      }
+    }
+
+    frameSkip.current++
+  })
+
   return (
-    <primitive
-      ref={group}
-      object={clone}
+    <group
       position={[
         (Math.cos(angle) * count * 2) / 3,
         0,
         (Math.sin(angle) * count * 2) / 3,
       ]}
       rotation={[0, rotationAngle, 0]}
-    />
+    >
+      <primitive ref={meshRef} object={clone} />
+    </group>
   )
 }
-
 
 function Platform({ count, duration }: { count: number; duration: number }) {
   const platformRef = useRef<THREE.Group>(null)
@@ -76,11 +95,11 @@ function Platform({ count, duration }: { count: number; duration: number }) {
     const time = state.clock.getElapsedTime()
 
     if (duration === 0) {
-      // console.error("Duration is 0, cannot calculate offset")
+      console.error("Duration is 0, cannot calculate offset")
       return
     }
 
-    const offset = PLATFORM_SPEED * 2 * Math.PI * time / duration
+    const offset = (PLATFORM_SPEED * 2 * Math.PI * time) / duration
     platformRef.current.rotation.y = offset
   })
 
--- src/stores/scene.ts
+++ src/stores/scene.ts
@@ -1,3 +1,3 @@
 export const TIME_SCALE = 0.45
 export const COUNT = 9
-export const PLATFORM_SPEED = -1 / COUNT
+export const PLATFORM_SPEED = -0.01 * COUNT
